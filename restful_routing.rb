ca2d7421ce9129e68f6be3384154030bec48d0fe
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-11-08 17:31:49 -0700

    posts

--- restful_routing.rb
+++ restful_routing.rb
@@ -1,5 +1,5 @@
      Prefix Verb   URI Pattern               Controller#Action
-users_index GET    /users/index(.:format)    users#index
+posts_index GET    /posts/index(.:format)    posts#index
     christi GET    /christi(.:format)        home#christi
       users GET    /users(.:format)          users#index
             POST   /users(.:format)          users#create
@@ -9,3 +9,11 @@ users_index GET    /users/index(.:format)    users#index
             PATCH  /users/:id(.:format)      users#update
             PUT    /users/:id(.:format)      users#update
             DELETE /users/:id(.:format)      users#destroy
+      posts GET    /posts(.:format)          posts#index
+            POST   /posts(.:format)          posts#create
+   new_post GET    /posts/new(.:format)      posts#new
+  edit_post GET    /posts/:id/edit(.:format) posts#edit
+       post GET    /posts/:id(.:format)      posts#show
+            PATCH  /posts/:id(.:format)      posts#update
+            PUT    /posts/:id(.:format)      posts#update
+            DELETE /posts/:id(.:format)      posts#destroy
